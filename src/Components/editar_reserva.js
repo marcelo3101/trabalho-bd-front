d824e0628588638264cc3320bbf49b02ef1a2511
map3101 <marcelo.postiglione@gmail.com>
2022-04-24 13:13:17 -0300

    editar jogo e reserva

---
+++ src/Components/editar_reserva.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"; 
+import api from "../api";
+
+const EditBooking = (reserva) => {
+    const [inicio, setInicio] = useState(reserva.data_hora_inicio);
+    const [fim, setFim] = useState(reserva.data_hora_termino);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        api.put(
+            `/reserva/${reserva.id}`,
+            {
+                "campo_id": campo,
+                "data_hora_inicio": inicio,
+                "data_hora_termino": fim,
+            }
+        ).then( () => {
+            setInicio("");
+            setFim("");
+        }
+        )
+    }
+    
+    return(
+        <div class="bg-white font-family-karla h-screen flex justify-center">
+           {!loading &&
+           <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
+                 <p class="text-center text-3xl">Editar Reserva</p>
+                 <form class="flex flex-col pt-3 md:pt-8" onSubmit={e => handleSubmit(e)}>
+                     <div class="flex justify-center">
+                      </div>
+                        <div class="flex flex-col pt-4">
+                         <label for="start-time" class="text-lg">Início</label>
+                         <input value={inicio} onChange={e => setInicio(e.target.value)} type="datetime-local" id="start-time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                        </div>
+                        <div class="flex flex-col pt-4">
+                         <label for="start-time" class="text-lg">Término</label>
+                         <input value={fim} onChange={e => setFim(e.target.value)} type="datetime-local" id="start-time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                        </div>
+                       <input type="submit" value="Salvar" class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" />
+                 </form>
+             </div>
+             }
+        </div>
+    )
+}
+
+export default EditUser;
