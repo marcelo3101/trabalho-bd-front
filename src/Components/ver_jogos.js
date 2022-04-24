ca374c2e24ae4de5e86824d2b3b3fea7e9aea65c
map3101 <marcelo.postiglione@gmail.com>
2022-04-24 12:42:32 -0300

    Ver jogos fix

--- src/Components/ver_jogos.js
+++ src/Components/ver_jogos.js
@@ -24,7 +24,7 @@ const SeeGames = () => {
             `/jogos/${id}`,
         ).then( () => {
             setLoading(true);
-            getjogos();
+            getJogos();
         }
         )
     }
